00c8fd61524e37b40c5130853865286026fa4691
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-10-30 15:46:21 +0300

    Убрал лишнюю аннотацию из EmplServImpl

--- src/main/java/ru/JD10/EmployeeBook/services/EmployeeServiceImpl.java
+++ src/main/java/ru/JD10/EmployeeBook/services/EmployeeServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@Repository
 public class EmployeeServiceImpl implements EmployeeService {
     public final Map<String, Employee> employees;
 
